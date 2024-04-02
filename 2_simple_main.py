d2ebab30dc9cf13d9138312f06d40093f8adbacd
Harry Kurniansyah <harry.kurniansyah@petrosea.com>
2024-04-02 10:56:33 +0700

    updating webdriver code in python

--- 2_simple_main.py
+++ 2_simple_main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,7 +16,7 @@ def get_top_10_search_results(keyword, headless=False):
         options.add_argument("--disable-dev-shm-usage")
 
     # Initialize the webdriver
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # driver = webdriver.Firefox(options=options)
 
     # Open Google and search for the keyword
